c8cd6c36235994dcd5e1a6dca965ad737cb814e9
falloumolina-glitch <falloumolina@gmail.com>
2026-04-12 23:33:28 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,28 @@
-const CACHE = "ia-tech-cache-v3";
+const CACHE = "ia-tech-v100";
 
 const FILES = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png"
+  "/ia-tech/",
+  "/ia-tech/index.html",
+  "/ia-tech/manifest.json",
+  "/ia-tech/icon-192.png",
+  "/ia-tech/icon-512.png"
 ];
 
-// INSTALL
 self.addEventListener("install", (event) => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(FILES))
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
 });
 
-// ACTIVATE
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((key) => {
-        if (key !== CACHE) return caches.delete(key);
-      }))
-    )
-  );
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
-// FETCH (IMPORTANT)
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((res) => {
-      return res || fetch(event.request).catch(() => caches.match("/index.html"));
-    })
+    fetch(event.request).catch(() =>
+      caches.match("/ia-tech/index.html")
+    )
   );
 });
